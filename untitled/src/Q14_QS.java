fc99368548ee92cb7a31738a72a24c00e0c20d94
pyu666 <32445749+pyu666@users.noreply.github.com>
2019-02-23 04:57:07 +0900

    🔥

--- untitled/src/Q14_QS.java
+++
@@ -1,8 +0,0 @@
-public class Q14_QS {
-    public static void main(String[] args){
-        int[] array = {20,56,55,74,3,45,13,87,46,30};
-        D12.print(array);
-        QuickSort.sort(array);
-        D12.print(array);
-    }
-}
